8e40d57c8dcc44fa025a84110262dd6bbce794ea
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-17 10:31:08 +0200

    Setup styling

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Link href='/sign-in'>Sign In</Link>
+      <Link href='/profile'>Profile</Link>
+      <Link href='/explore'>Explore</Link>
+      <Link href='/properties/7'>Property Details</Link>
     </View>
   );
 }

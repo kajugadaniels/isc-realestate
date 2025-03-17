1a3071e7c129562a8ad361268f7c31324dbf7bfa
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-17 14:10:15 +0200

    Design home nav header

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,15 +1,34 @@
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View>
-      <Text className="font-bold font-rubik">Welcome</Text>
-      <Link href='/profile'>Profile</Link>
-      <Link href='/explore'>Explore</Link>
-      <Link href='/properties/7'>Property Details</Link>
-      <Link href='/sign-in'>Sign In</Link>
-      <Link href='/sign-up'>Sign Up</Link>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image
+              source={images.avatar}
+              className="size-12 rounded-full"
+            />
+
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-xs font-rubik text-black-100">
+                Good Morning
+              </Text>
+              <Text className="text-base font-rubik-medium text-black-300">
+                John Doe
+              </Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }

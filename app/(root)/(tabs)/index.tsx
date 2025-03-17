41bb50a9ba02304cec6138b5c1470674605a87b4
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-17 11:04:43 +0200

    Setup assets and font

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text className="font-bold text-lg">Welcome</Text>
+      <Text className="font-bold font-rubik">Welcome</Text>
       <Link href='/sign-in'>Sign In</Link>
       <Link href='/profile'>Profile</Link>
       <Link href='/explore'>Explore</Link>

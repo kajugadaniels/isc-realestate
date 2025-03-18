7b6f7233d50c74b2fce8fb359099606f382026d8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-18 10:00:55 +0200

    New changes

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -14,40 +14,42 @@ export const FeaturedCard = ({ onPress }: Props) => {
       onPress={onPress}
       className="flex flex-col items-start w-60 h-80 relative"
     >
-      <Image source={images.japan} className='size-full rounded-2xl' />
+      <Link href="/properties/1">
+        <Image source={images.japan} className='size-full rounded-2xl' />
 
-      <Image
-        source={images.cardGradient}
-        className="size-full rounded-2xl absolute bottom-0"
-      />
+        <Image
+          source={images.cardGradient}
+          className="size-full rounded-2xl absolute bottom-0"
+        />
 
-      <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
-        <Image source={icons.star} className="size-3.5" />
-        <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
-          7.8
-        </Text>
-      </View>
-
-      <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
-        <Link href="/properties/1">
-          <Text
-            className="text-xl font-rubik-extrabold text-white"
-            numberOfLines={1}
-          >
-            Property Name
+        <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
+          <Image source={icons.star} className="size-3.5" />
+          <Text className="text-xs font-rubik-bold text-primary-300 ml-1">
+            7.8
           </Text>
-        </Link>
-        <Text className="text-base font-rubik text-white" numberOfLines={1}>
-          Kigali Rwanda
-        </Text>
-
-        <View className="flex flex-row items-center justify-between w-full">
-          <Text className="text-xl font-rubik-extrabold text-white">
-            $200
-          </Text>
-          <Image source={icons.heart} className="size-5" />
         </View>
-      </View>
+
+        <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
+          <Link href="/properties/1">
+            <Text
+              className="text-xl font-rubik-extrabold text-white"
+              numberOfLines={1}
+            >
+              Property Name
+            </Text>
+          </Link>
+          <Text className="text-base font-rubik text-white" numberOfLines={1}>
+            Kigali Rwanda
+          </Text>
+
+          <View className="flex flex-row items-center justify-between w-full">
+            <Text className="text-xl font-rubik-extrabold text-white">
+              $200
+            </Text>
+            <Image source={icons.heart} className="size-5" />
+          </View>
+        </View>
+      </Link>
     </TouchableOpacity>
   )
 }
@@ -55,38 +57,38 @@ export const FeaturedCard = ({ onPress }: Props) => {
 export const Card = () => {
   return (
     <TouchableOpacity
-      className="flex flex-col items-start w-60 h-80 relative"
+      className="flex flex-col items-start w-44 h-64 relative"
     >
-      <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
-        <Image source={icons.star} className="size-2.5" />
-        <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
-          4.3
-        </Text>
-      </View>
+      <Link href="/properties/1">
+        <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+          <Image source={icons.star} className="size-2.5" />
+          <Text className="text-xs font-rubik-bold text-primary-300 ml-0.5">
+            4.3
+          </Text>
+        </View>
 
-      <Image source={images.japan} className="w-full h-40 rounded-lg" />
+        <Image source={images.newYork} className="w-full h-40 rounded-lg" />
 
-      <View className="flex flex-col mt-2">
-        <Link href="/properties/1">
+        <View className="flex flex-col mt-2">
           <Text className="text-base font-rubik-bold text-black-300">
             Property Name
           </Text>
-        </Link>
-        <Text className="text-xs font-rubik text-black-100">
-          Kigali Rwanda
-        </Text>
-
-        <View className="flex flex-row items-center justify-between mt-2">
-          <Text className="text-base font-rubik-bold text-primary-300">
-            $140
+          <Text className="text-xs font-rubik text-black-100">
+            Kigali Rwanda
           </Text>
-          <Image
-            source={icons.heart}
-            className="w-5 h-5 mr-2"
-            tintColor="#191D31"
-          />
+
+          <View className="flex flex-row items-center justify-between mt-2">
+            <Text className="text-base font-rubik-bold text-primary-300">
+              $140
+            </Text>
+            <Image
+              source={icons.heart}
+              className="w-5 h-5 mr-2"
+              tintColor="#191D31"
+            />
+          </View>
         </View>
-      </View>
+      </Link>
     </TouchableOpacity>
   )
 }

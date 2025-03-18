dc745e55a7fdc51ac6311a4b013963588f12a2d8
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-18 09:18:58 +0200

    Design explores page

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,12 +1,54 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Card, FeaturedCard } from "@/components/Cards";
+import Filters from "@/components/Filters";
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Link } from "expo-router";
+import { FlatList, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const Explore = () => {
+export default function Explore() {
   return (
-    <View>
-      <Text>Explore</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="h-full bg-white">
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={({item}) => <Card />}
+        keyExtractor={(item) => item.toLocaleString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row">
+                <Image
+                  source={images.avatar}
+                  className="size-12 rounded-full"
+                />
 
-export default Explore
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base font-rubik-medium text-black-300">
+                    John Doe
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
+            </View>
+
+            <Search />
+
+            <View className="mt-5">
+              <Text>Found 16 properties</Text>
+            </View>
+
+            <Filters />
+          </View>
+        }
+      />
+    </SafeAreaView>
+  );
+}
